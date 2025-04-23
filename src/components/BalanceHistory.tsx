c5832d33fa5918958e0b4c1ae80b8f18da15fc89
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2025-04-23 20:23:26 +0100

    style BalanceHistory

--- src/components/BalanceHistory.tsx
+++ src/components/BalanceHistory.tsx
@@ -1,4 +1,3 @@
-import React, { PureComponent } from "react";
 import {
   AreaChart,
   Area,
@@ -11,80 +10,81 @@ import {
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
+    name: "Jan",
     amt: 2400,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    name: "Feb",
+    amt: 2800,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
+    name: "Mar",
     amt: 2000,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    name: "Apr",
+    amt: 3000,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    name: "May",
+    amt: 1500,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    name: "Jun",
+    amt: 3500,
+  },
+  {
+    name: "Jul",
+    amt: 1800,
   },
 ];
 
-export default class BalanceHistoryChart extends PureComponent {
-  static demoUrl = "https://codesandbox.io/p/sandbox/simple-area-chart-4y9cnl";
+const BalanceHistoryChart = () => {
+  return (
+    <div className="p-2 sm:p-4 sm:py-7 bg-white sm:rounded-4xl h-[214px] sm:h-[276px]">
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <defs>
+            <linearGradient id="colorAmt" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#2D60FF" stopOpacity={0.5} />
+              <stop offset="95%" stopColor="#2D60FF" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid strokeDasharray="3 3" stroke="#DFE5EE" />
+          <XAxis
+            dataKey="name"
+            tick={{ fill: "#718EBF" }}
+            axisLine={{ stroke: "#718EBF" }}
+            fontSize={13}
+          />
+          <YAxis
+            color="#718EBF"
+            tick={{ fill: "#718EBF" }}
+            fontSize={13}
+            axisLine={{ stroke: "#718EBF" }}
+          />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="amt"
+            stroke="#1814F3"
+            strokeWidth={3}
+            fill="url(#colorAmt)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="p-2 sm:p-4 bg-white sm:rounded-4xl h-[214px] sm:h-[276px]">
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={500}
-            height={400}
-            data={data}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="uv"
-              stroke="#8884d8"
-              fill="#8884d8"
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
-    );
-  }
-}
+export default BalanceHistoryChart;
