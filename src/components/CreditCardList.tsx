f1310998b88a64c443b167207dd61b24fd543828
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2025-04-24 23:26:16 +0100

    add see all to cardslist

--- src/components/CreditCardList.tsx
+++ src/components/CreditCardList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CreditCard from "./CreditCard";
 import { useGetCardsQuery } from "../apiSlice";
+import SectionTitle from "./SectionTitle";
 
 type CreditCardType = "black" | "white";
 
@@ -11,17 +12,29 @@ const CreditCardList: React.FC = () => {
   if (error) return <div>Error: {error.toString()}</div>;
 
   return (
-    <div className="w-full flex-wrap overflow-x-auto">
-      <div className="w-full flex gap-4">
-        {cards?.data?.map((card: any, index: number) => (
-          <CreditCard
-            key={index}
-            details={card.details}
-            type={card.type as CreditCardType}
-          />
-        ))}
+    <section>
+      <div className="flex justify-between items-baseline">
+        <SectionTitle title="My Cards" />
+        <button className="text-[17px] font-semibold text-soar-dark-blue">
+          See All
+        </button>
       </div>
-    </div>
+      <div className="w-full">
+        <div className="w-full flex-wrap overflow-x-auto">
+          <div className="w-full flex gap-6">
+            {[cards?.data?.[0], cards?.data?.[1]].map(
+              (card: any, index: number) => (
+                <CreditCard
+                  key={index}
+                  details={card.details}
+                  type={card.type as CreditCardType}
+                />
+              ),
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
