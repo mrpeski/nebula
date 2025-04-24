6c0cc770a7c0e8527a5bd4b7375cb8f28a31a215
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2025-04-24 08:44:24 +0100

    style topbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,14 @@
 import { SidebarProvider } from "./SidebarContext";
 import { Menu, MenuButton } from "@headlessui/react";
-import { BellIcon, CogIcon } from "@heroicons/react/24/outline";
+import bellIcon from "../assets/bell.svg";
+import cogIcon from "../assets/settings-line.svg";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import StaticSidebar from "./StaticSidebar";
 import DynamicSidebar from "./DynamicSidebar";
 import SidebarToggle from "./SidebarToggle";
 import userAvatar from "../assets/user.png";
 import PageTitle from "../components/PageTitle";
+import React from "react";
 
 interface Props {
   title: string;
@@ -21,23 +23,23 @@ export default function Layout(props: Props) {
 
         <div className="lg:pl-[250px]">
           <div className="border-gray-200 sm:border-b lg:mx-auto lg:max-w-7xl lg:px-8">
-            <div className="flex py-5 items-center gap-x-4  bg-white px-4 sm:gap-x-6 sm:px-6 lg:px-0">
+            <div className="flex items-center gap-x-4  bg-white px-4 sm:gap-x-6 sm:px-6 lg:px-0">
               <SidebarToggle />
-              <div className="flex flex-1 gap-x-4 justify-between items-center lg:gap-x-6">
+              <div className="flex flex-1 gap-x-4 justify-between items-center lg:gap-x-6 sm:h-[100px]">
                 <h3 className="flex-1 text-center sm:text-left">
                   <PageTitle title={props.title} />
                 </h3>
                 <form
                   action="#"
                   method="GET"
-                  className="hidden sm:grid grid-cols-1 bg-gray-200"
+                  className="hidden sm:grid grid-cols-1 bg-gray-100 p-4 rounded-full"
                 >
                   <input
                     name="search"
                     type="search"
                     placeholder="Search"
                     aria-label="Search"
-                    className="col-start-1 row-start-1 block size-full bg-white pl-8 text-base text-gray-900 outline-hidden placeholder:text-gray-400 sm:text-sm/6"
+                    className="col-start-1 row-start-1 block size-full pl-8 text-base text-gray-900 outline-hidden placeholder:text-gray-400 sm:text-sm/6 bg-transparent"
                   />
                   <MagnifyingGlassIcon
                     aria-hidden="true"
@@ -45,28 +47,22 @@ export default function Layout(props: Props) {
                   />
                 </form>
 
-                <div className="flex items-center gap-x-4 lg:gap-x-6">
+                <div className="flex items-center gap-x- lg:gap-x-6">
                   <button
                     type="button"
-                    className="hidden sm:inline-block -m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+                    className="hidden sm:inline-block -m-2.5 p-2.5 text-gray-400 hover:text-gray-500 bg-gray-100 rounded-full"
                   >
                     <span className="sr-only">Settings</span>
-                    <CogIcon aria-hidden="true" className="size-6" />
+                    <img alt="settings" src={cogIcon} />
                   </button>
                   <button
                     type="button"
-                    className=" hidden sm:inline-block -m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+                    className=" hidden sm:inline-block -m-2.5 p-2.5 text-gray-400 hover:text-gray-500 bg-gray-100 rounded-full"
                   >
                     <span className="sr-only">View notifications</span>
-                    <BellIcon aria-hidden="true" className="size-6" />
+                    <img alt="notifications" src={bellIcon} />
                   </button>
-                  {/* Separator */}
-                  <div
-                    aria-hidden="true"
-                    className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-200"
-                  />
 
-                  {/* Profile dropdown */}
                   <Menu as="div" className="relative">
                     <MenuButton className="-m-1.5 flex items-center p-1.5">
                       <span className="sr-only">Open user menu</span>
